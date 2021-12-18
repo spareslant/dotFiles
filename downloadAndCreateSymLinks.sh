4c806f1733f304f4962a99ce2976405eebd49d0e
Gaurav Pal <spare.slant@gmail.com>
2021-12-18 14:08:20 +0000

    tabspace=4

--- downloadAndCreateSymLinks.sh
+++ downloadAndCreateSymLinks.sh
@@ -11,186 +11,186 @@ mkdir -p $THIS_SCRIPT_LOCATION/downloaded
 mkdir -p $THIS_SCRIPT_LOCATION/rendered_configs
 
 function setupCommonSwapDir() {
-  mkdir -p $HOME/.vim/swapfiles/
+    mkdir -p $HOME/.vim/swapfiles/
 }
 
 function _updateOrCloneGitRepo() {
-  local path="$1"
-  shift
-  local urls=("$@")
-  local all_dirs_in_urls=("")
-  pushd "${path}"
-    for url in "${urls[@]}"
-    do
-      local dirName=$(echo $url | perl -wnl -e 'm!.+/(.+)\.git! and print "$1"')
-      all_dirs_in_urls=(${all_dirs_in_urls[@]} $dirName)
-      if [[ -d ${dirName} ]]
-      then
-        pushd ${path}/${dirName}
-          git reset --hard
-          git pull -r
-        popd || exit 1
-      else
-        git clone ${url}
-      fi
-    done
-  popd
+    local path="$1"
+    shift
+    local urls=("$@")
+    local all_dirs_in_urls=("")
+    pushd "${path}"
+        for url in "${urls[@]}"
+        do
+            local dirName=$(echo $url | perl -wnl -e 'm!.+/(.+)\.git! and print "$1"')
+            all_dirs_in_urls=(${all_dirs_in_urls[@]} $dirName)
+            if [[ -d ${dirName} ]]
+            then
+                pushd ${path}/${dirName}
+                    git reset --hard
+                    git pull -r
+                popd || exit 1
+            else
+                git clone ${url}
+            fi
+        done
+    popd
 
-  # remove unwanted vim and zsh plugin repos
-  if echo "$path" | egrep -q 'vim-plugins|ohmyzsh/custom/plugins'
-  then
-    local dirs_in_path=($(/bin/ls -1 ${path}))
-    for dir in "${dirs_in_path[@]}"
-    do
-      local not_found="true"
-      for url_dir in "${all_dirs_in_urls[@]}"
-      do
-        if [[ "$url_dir" == "$dir" ]]
-        then
-            not_found="false"
-            break
-        fi
-      done
-      if [[ "$not_found" == "true" ]]  
-      then
-        echo "WARNING: unwanted plugin ${path}/${dir} will be removed."
-        rm -rfv ${path}/${dir} 
-        if echo "$path" | egrep -q 'vim-plugins'
-        then
-          rm -f ~/.vim/pack/plugins/start/${dir}
-        fi
-      fi
-    done
-  fi
+    # remove unwanted vim and zsh plugin repos
+    if echo "$path" | egrep -q 'vim-plugins|ohmyzsh/custom/plugins'
+    then
+        local dirs_in_path=($(/bin/ls -1 ${path}))
+        for dir in "${dirs_in_path[@]}"
+        do
+            local not_found="true"
+            for url_dir in "${all_dirs_in_urls[@]}"
+            do
+                if [[ "$url_dir" == "$dir" ]]
+                then
+                        not_found="false"
+                        break
+                fi
+            done
+            if [[ "$not_found" == "true" ]]  
+            then
+                echo "WARNING: unwanted plugin ${path}/${dir} will be removed."
+                rm -rfv ${path}/${dir} 
+                if echo "$path" | egrep -q 'vim-plugins'
+                then
+                    rm -f ~/.vim/pack/plugins/start/${dir}
+                fi
+            fi
+        done
+    fi
 }
 
 function downloadVimPlugins() {
-  echo "INFO: Downloading plugins...."
-  local plugins=(
-    https://github.com/vim-airline/vim-airline.git
-    https://github.com/jiangmiao/auto-pairs.git
-    https://github.com/junegunn/fzf.vim.git
-    https://github.com/jesseleite/vim-agriculture.git
-    https://github.com/neoclide/coc.nvim.git
-    https://github.com/airblade/vim-gitgutter.git
-    https://github.com/vim-airline/vim-airline-themes.git
-    https://github.com/voldikss/vim-floaterm.git
-    https://github.com/mcchrish/nnn.vim.git
-    https://github.com/Yggdroot/indentLine.git
-    https://github.com/chriskempson/base16-vim.git
-    https://github.com/saltstack/salt-vim.git
-    https://github.com/stephpy/vim-yaml.git
-    https://github.com/lepture/vim-jinja.git
-    https://github.com/hashivim/vim-terraform.git
-  )
-  mkdir -p ~/.vim/pack/plugins/start
-  mkdir -p $THIS_SCRIPT_LOCATION/downloaded/vim-plugins
-  _updateOrCloneGitRepo "${THIS_SCRIPT_LOCATION}/downloaded/vim-plugins" "${plugins[@]}"
-  echo "INFO: Creating vimrc and vim plugin links..."
-  ln -svf $THIS_SCRIPT_LOCATION/downloaded/vim-plugins/* ~/.vim/pack/plugins/start
-  ln -svf $THIS_SCRIPT_LOCATION/vim/vimrc ~/.vimrc
+    echo "INFO: Downloading plugins...."
+    local plugins=(
+        https://github.com/vim-airline/vim-airline.git
+        https://github.com/jiangmiao/auto-pairs.git
+        https://github.com/junegunn/fzf.vim.git
+        https://github.com/jesseleite/vim-agriculture.git
+        https://github.com/neoclide/coc.nvim.git
+        https://github.com/airblade/vim-gitgutter.git
+        https://github.com/vim-airline/vim-airline-themes.git
+        https://github.com/voldikss/vim-floaterm.git
+        https://github.com/mcchrish/nnn.vim.git
+        https://github.com/Yggdroot/indentLine.git
+        https://github.com/chriskempson/base16-vim.git
+        https://github.com/saltstack/salt-vim.git
+        https://github.com/stephpy/vim-yaml.git
+        https://github.com/lepture/vim-jinja.git
+        https://github.com/hashivim/vim-terraform.git
+    )
+    mkdir -p ~/.vim/pack/plugins/start
+    mkdir -p $THIS_SCRIPT_LOCATION/downloaded/vim-plugins
+    _updateOrCloneGitRepo "${THIS_SCRIPT_LOCATION}/downloaded/vim-plugins" "${plugins[@]}"
+    echo "INFO: Creating vimrc and vim plugin links..."
+    ln -svf $THIS_SCRIPT_LOCATION/downloaded/vim-plugins/* ~/.vim/pack/plugins/start
+    ln -svf $THIS_SCRIPT_LOCATION/vim/vimrc ~/.vimrc
 }
 
 function installPowerlineFonts() {
-  echo "INFO: Installing powerline fonts....."
-  # there is uninstall script as well in the repository.
-  pushd $THIS_SCRIPT_LOCATION/downloaded
-    if [[ -d powerLineFonts ]]
-    then
-      pushd powerLineFonts
-        git pull -r
-      popd || exit 1
-    else
-      git clone https://github.com/powerline/fonts.git --depth=1 powerLineFonts
-      pushd powerLineFonts
-      ./install.sh
-    fi
-  popd || exit 1
+    echo "INFO: Installing powerline fonts....."
+    # there is uninstall script as well in the repository.
+    pushd $THIS_SCRIPT_LOCATION/downloaded
+        if [[ -d powerLineFonts ]]
+        then
+            pushd powerLineFonts
+                git pull -r
+            popd || exit 1
+        else
+            git clone https://github.com/powerline/fonts.git --depth=1 powerLineFonts
+            pushd powerLineFonts
+            ./install.sh
+        fi
+    popd || exit 1
 }
 
 function installZsh() {
-  local plugin="https://github.com/ohmyzsh/ohmyzsh.git"
-  local zshUrls=(
-    "https://github.com/ohmyzsh/ohmyzsh.git"
-  )
-  _updateOrCloneGitRepo "$THIS_SCRIPT_LOCATION/downloaded" "${zshUrls[@]}"
-  if [[ -e "${HOME}/.oh-my-zsh" && ! -L "${HOME}/.oh-my-zsh" ]]
-  then
-    local backupName="oh-my-zsh.backup.$(date +%Y-%m-%d-%H:%M:%S)"
-    echo "WARNING: Found ${HOME}/.oh-my-zsh, backing it up as ${backupName}..."
-    mv "${HOME}/.oh-my-zsh" "${HOME}/${backupName}"
-  fi
-  ln -svf $THIS_SCRIPT_LOCATION/downloaded/ohmyzsh ~/.oh-my-zsh
-  ln -svf $THIS_SCRIPT_LOCATION/zsh/zshrc ~/.zshrc
-  ln -svf $THIS_SCRIPT_LOCATION/zsh/zprofile ~/.zprofile
-  ln -svf $THIS_SCRIPT_LOCATION/zsh/gpal.zsh-theme $THIS_SCRIPT_LOCATION/downloaded/ohmyzsh/themes/
+    local plugin="https://github.com/ohmyzsh/ohmyzsh.git"
+    local zshUrls=(
+        "https://github.com/ohmyzsh/ohmyzsh.git"
+    )
+    _updateOrCloneGitRepo "$THIS_SCRIPT_LOCATION/downloaded" "${zshUrls[@]}"
+    if [[ -e "${HOME}/.oh-my-zsh" && ! -L "${HOME}/.oh-my-zsh" ]]
+    then
+        local backupName="oh-my-zsh.backup.$(date +%Y-%m-%d-%H:%M:%S)"
+        echo "WARNING: Found ${HOME}/.oh-my-zsh, backing it up as ${backupName}..."
+        mv "${HOME}/.oh-my-zsh" "${HOME}/${backupName}"
+    fi
+    ln -svf $THIS_SCRIPT_LOCATION/downloaded/ohmyzsh ~/.oh-my-zsh
+    ln -svf $THIS_SCRIPT_LOCATION/zsh/zshrc ~/.zshrc
+    ln -svf $THIS_SCRIPT_LOCATION/zsh/zprofile ~/.zprofile
+    ln -svf $THIS_SCRIPT_LOCATION/zsh/gpal.zsh-theme $THIS_SCRIPT_LOCATION/downloaded/ohmyzsh/themes/
 }
 
 function installZshPlugins() {
-  echo "INFO: downloading zsh plugins..."
-  local plugins=(
-    "https://github.com/zsh-users/zsh-autosuggestions.git"
-    "https://github.com/zsh-users/zsh-syntax-highlighting.git"
-    "https://github.com/zsh-users/zsh-completions.git"
-    "https://github.com/zsh-users/zsh-history-substring-search.git"
-  )
-  _updateOrCloneGitRepo "${THIS_SCRIPT_LOCATION}/downloaded/ohmyzsh/custom/plugins" "${plugins[@]}"
+    echo "INFO: downloading zsh plugins..."
+    local plugins=(
+        "https://github.com/zsh-users/zsh-autosuggestions.git"
+        "https://github.com/zsh-users/zsh-syntax-highlighting.git"
+        "https://github.com/zsh-users/zsh-completions.git"
+        "https://github.com/zsh-users/zsh-history-substring-search.git"
+    )
+    _updateOrCloneGitRepo "${THIS_SCRIPT_LOCATION}/downloaded/ohmyzsh/custom/plugins" "${plugins[@]}"
 }
 
 function downloadExtraShellUtilities() {
-  echo "INFO: downloading extra shell utilities..."
-  local plugins=(
-    "https://github.com/bigH/git-fuzzy.git"
-    "https://github.com/jarun/nnn.git"
-    "https://github.com/chriskempson/base16-shell.git"
-    "https://github.com/mattdavis90/base16-tmux.git"
-  )
-  mkdir -p "${THIS_SCRIPT_LOCATION}/downloaded/extraShellUtilities"
-  _updateOrCloneGitRepo "${THIS_SCRIPT_LOCATION}/downloaded/extraShellUtilities" "${plugins[@]}"
+    echo "INFO: downloading extra shell utilities..."
+    local plugins=(
+        "https://github.com/bigH/git-fuzzy.git"
+        "https://github.com/jarun/nnn.git"
+        "https://github.com/chriskempson/base16-shell.git"
+        "https://github.com/mattdavis90/base16-tmux.git"
+    )
+    mkdir -p "${THIS_SCRIPT_LOCATION}/downloaded/extraShellUtilities"
+    _updateOrCloneGitRepo "${THIS_SCRIPT_LOCATION}/downloaded/extraShellUtilities" "${plugins[@]}"
 }
 
 function compileNNNandInstall() {
-  pushd "${THIS_SCRIPT_LOCATION}/downloaded/extraShellUtilities/nnn"
-    echo "INFO: Patching nnn.c to use diamond indicator in detail mode..."
-    sed -i -e 's/ACS_CKBOARD/ACS_DIAMOND/g' src/nnn.c
-    make clean
-    make O_PCRE=1 O_NERD=1 O_CKBOARD=1
-    cp -v -f nnn /usr/local/bin/
-    make clean
-  popd
-  rm -rf "${THIS_SCRIPT_LOCATION}/downloaded/extraShellUtilities/nnn"
-  echo "Install nnn plugins..."
-  rm -rf $HOME/.config/nnn
-  curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
-  echo "INFO: installing custom nnn plugin..."
-  ln -svf $THIS_SCRIPT_LOCATION/nnn/fz-preview-open $HOME/.config/nnn/plugins/
-  ln -svf $THIS_SCRIPT_LOCATION/nnn/fz-preview-cd $HOME/.config/nnn/plugins/
+    pushd "${THIS_SCRIPT_LOCATION}/downloaded/extraShellUtilities/nnn"
+        echo "INFO: Patching nnn.c to use diamond indicator in detail mode..."
+        sed -i -e 's/ACS_CKBOARD/ACS_DIAMOND/g' src/nnn.c
+        make clean
+        make O_PCRE=1 O_NERD=1 O_CKBOARD=1
+        cp -v -f nnn /usr/local/bin/
+        make clean
+    popd
+    rm -rf "${THIS_SCRIPT_LOCATION}/downloaded/extraShellUtilities/nnn"
+    echo "Install nnn plugins..."
+    rm -rf $HOME/.config/nnn
+    curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
+    echo "INFO: installing custom nnn plugin..."
+    ln -svf $THIS_SCRIPT_LOCATION/nnn/fz-preview-open $HOME/.config/nnn/plugins/
+    ln -svf $THIS_SCRIPT_LOCATION/nnn/fz-preview-cd $HOME/.config/nnn/plugins/
 }
 
 function createLinks() {
-  ln -svf $THIS_SCRIPT_LOCATION/git_configs/gitconfig ~/.gitconfig
-  ln -svf $THIS_SCRIPT_LOCATION/rendered_configs/tmux.conf ~/.tmux.conf
+    ln -svf $THIS_SCRIPT_LOCATION/git_configs/gitconfig ~/.gitconfig
+    ln -svf $THIS_SCRIPT_LOCATION/rendered_configs/tmux.conf ~/.tmux.conf
 }
 
 function patchNetrwPlugin() {
-  echo "patch netrw.vim file for smooth vertical lines "
-  vimRunTimePath=$(vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015')
-  if [[ ! -e "${vimRunTimePath}/autoload/netrw.vim" ]]
-  then
-    echo "ERROR: could not find ${vimRunTimePath}/autoload/netrw.vim file.."
-    exit 2
-  fi
-  timeStamp="$(date +%Y-%m-%d-%H:%M:%S)"
-  backupFile="${vimRunTimePath}/autoload/netrw.vim".backup.${timeStamp}
-  patchedFile="/tmp/pacthedFile.${timeStamp}"
-  perl -wnl -e 'm!(.*let s:treedepthstring=)! and print "$1 \"│ \"" or print' "${vimRunTimePath}/autoload/netrw.vim" > ${patchedFile}
-  if diff -b ${patchedFile} "${vimRunTimePath}/autoload/netrw.vim" > /dev/null 2>&1
-  then
-    echo "INFO: ${vimRunTimePath}/autoload/netrw.vim already pacthed. Skipping this patch.."
-  else
-    cp -pv "${vimRunTimePath}/autoload/netrw.vim" "${backupFile}"
-    cp -vf "${patchedFile}" "${vimRunTimePath}/autoload/netrw.vim"
-  fi
+    echo "patch netrw.vim file for smooth vertical lines "
+    vimRunTimePath=$(vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015')
+    if [[ ! -e "${vimRunTimePath}/autoload/netrw.vim" ]]
+    then
+        echo "ERROR: could not find ${vimRunTimePath}/autoload/netrw.vim file.."
+        exit 2
+    fi
+    timeStamp="$(date +%Y-%m-%d-%H:%M:%S)"
+    backupFile="${vimRunTimePath}/autoload/netrw.vim".backup.${timeStamp}
+    patchedFile="/tmp/pacthedFile.${timeStamp}"
+    perl -wnl -e 'm!(.*let s:treedepthstring=)! and print "$1 \"│ \"" or print' "${vimRunTimePath}/autoload/netrw.vim" > ${patchedFile}
+    if diff -b ${patchedFile} "${vimRunTimePath}/autoload/netrw.vim" > /dev/null 2>&1
+    then
+        echo "INFO: ${vimRunTimePath}/autoload/netrw.vim already pacthed. Skipping this patch.."
+    else
+        cp -pv "${vimRunTimePath}/autoload/netrw.vim" "${backupFile}"
+        cp -vf "${patchedFile}" "${vimRunTimePath}/autoload/netrw.vim"
+    fi
 
 }
 
