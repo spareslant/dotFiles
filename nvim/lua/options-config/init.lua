76b119d6bd142689c698d217359207fa5d8b468d
Gaurav Pal <spare.slant@gmail.com>
2021-12-25 20:29:40 +0000

    fixed tabspace=2

--- nvim/lua/options-config/init.lua
+++ nvim/lua/options-config/init.lua
@@ -2,13 +2,12 @@ local set = vim.opt
 
 vim.notify = require("notify")
 
-set.shiftwidth = 4
-set.tabstop = 4
-set.softtabstop = 4
+set.shiftwidth = 2
+set.tabstop = 2
+set.softtabstop = 2
 set.scrolloff = 5
 set.fileencoding = 'utf-8'
 set.termguicolors = true
 set.cursorline = true
 set.number = true
 set.mouse = 'a'
-set.scrolloff = 3
