dd84b10b76d1d758737f817639e234f692a843f7
Gaurav Pal <spare.slant@gmail.com>
2022-09-04 09:02:18 +0100

    wrap telescope results

--- nvim/lua/telescope-config/init.lua
+++ nvim/lua/telescope-config/init.lua
@@ -4,6 +4,10 @@ local trouble = require("trouble.providers.telescope")
 
 local telescope = require("telescope")
 
+vim.cmd [[
+  autocmd User TelescopePreviewerLoaded setlocal wrap
+]]
+
 telescope.setup({
   defaults = {
     mappings = {
