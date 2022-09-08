4093db5bd4cba4497a918320a123f82806f03833
Gaurav Pal <spare.slant@gmail.com>
2022-09-08 23:33:06 +0100

    fixed cokeline color - 2

--- nvim/lua/cokeline-config/init.lua
+++ nvim/lua/cokeline-config/init.lua
@@ -1,5 +1,5 @@
 local get_hex = require("cokeline.utils").get_hex
-local active_bg_color = '#5c575f'
+local active_bg_color = '#393939'
 local inactive_bg_color = get_hex('Normal', 'bg')
 local bg_color = get_hex('ColorColumn', 'bg')
 local is_picking_focus = require('cokeline/mappings').is_picking_focus
@@ -111,7 +111,7 @@ require('cokeline').setup({
                   if(buffer.diagnostics.errors > 0) then
                       return '#C95157'
                   else
-                      return '#d5d1d4'
+                      return '#b3b1ad'
                   end
               end,
               style = function(buffer)
