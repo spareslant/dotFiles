e6110a3108728fe3bb6a2b4db191d46352966e5b
Gaurav Pal <spare.slant@gmail.com>
2022-09-08 10:01:41 +0100

    commented unused functions

--- zsh/zsh_user_functions.zsh
+++ zsh/zsh_user_functions.zsh
@@ -24,6 +24,78 @@ export FZF_DEFAULT_OPTS="--prompt='[!]filter: ᯼ ⫸  ' --pointer='⮀' --marke
     --color gutter:232 \
     --preview-window='wrap'"
 
+# function fzf_bin() {
+#     header="⟰ $(pwd)"
+#     fzf --header="$header" "$@"
+# }
+# 
+# # ==== Open file/dir. Based on type of file it either cd to directory of opens file in vim when <Enter> is pressed
+# function fzOpenFileOrDir() {
+#     fzf_bin --preview="source $UTILITY_SCRIPT; previewFileOrDir {}" --preview-window='wrap' | openFileOrDir
+# }
+# 
+# # ==== Preview file/dir. Selected files will also be copied in system clipboard
+# function fzPreviewFilesOrDirsAndCopy() {
+#     #fzf_bin --multi --preview="source $UTILITY_SCRIPT; previewFileOrDir {}" --preview-window='wrap' --bind 'ctrl-o:execute(vim -R -o {})' | tee >(pbcopy)
+#     fzf_bin --multi --preview="source $UTILITY_SCRIPT; previewFileOrDir {}" --preview-window='wrap' | tee >(pbcopy)
+# }
+# 
+# # ==== Directory listing and switching
+# function fzChangeDir() {
+#     fd -t d -H -I --exclude .git | fzf_bin --preview="source $UTILITY_SCRIPT; previewFileOrDir {}" --preview-window='wrap' | openFileOrDir
+# }
+# 
+# # ==== Open multiple files in vim in their own respective windows. (Note: use :only in vim to all others)
+# function fzOpenMultiFilesInVim() {
+#     local filesList=$(mktemp -t fzf)
+#     fd -H -I -t f --exclude .git | fzf_bin --multi --preview="source $UTILITY_SCRIPT; previewFileOrDir {}" --preview-window='wrap' > "$filesList"
+#     vim -o $(cat "$filesList")
+#     rm -f "$filesList"
+# }
+# 
+# # ==== Changes the directory from current directory ownwards and also previews its underneath tree structure.
+# #function fzcd() {
+# #  cd $(fd -t d -H -I --exclude .git | fzf_bin--preview='tree -C -L 2 {}' --preview-window='wrap') && echo $PWD
+# #}
+# 
+# # ==== Changes the directory from HOME directory ownwards and also previews its underneath tree structure.
+# function fzChangeDirFromHome() {
+#     cd $(fd -t d -H -I --exclude .git '' $HOME/ | fzf_bin --preview='tree -C -L 2 {}' --preview-window='wrap') && echo $PWD
+# }
+# 
+# # ==== Grep word from all files in current directory and passes onto fzf for selection. Selected files will also be copied in system clipboard
+# function fzPreviewGreppedFiles() {
+#     [[ $# -eq 0 ]] && echo "usage is ./fzOperateOnAllGreppedFiles [rg-options] <string>" && return 1
+#     local item=$@
+#     rg --no-messages "$@" -l | fzf_bin --multi --preview-window='wrap' --preview="source $UTILITY_SCRIPT; previewFileOrDir {} | rg $item --color=always -C 2 --context-separator='~~~~~~~~~ SKIPPED CONTENT ~~~~~~~~~' " | tee >(pbcopy)
+# }
+# 
+# # ==== Do operation interactively on grepped files in vim. Additionaly select grepped files via fzf
+# function fzOperateOnSelectedGreppedFiles() {
+#     [[ $# -eq 0 ]] && echo "usage is ./fzOperateOnAllGreppedFiles [rg-options] <string>" && return 2
+#     local quickFixFile=$(mktemp -t fzf)
+#     rg --no-messages "$@" --vimgrep $(fzPreviewGreppedFiles "$@") > "$quickFixFile"
+#     vim -q "$quickFixFile" -c ":copen"
+#     rm -f "$quickFixFile"
+# }
+# 
+# # ==== Do operation interactively on grepped files in vim
+# function fzOperateOnGreppedFiles() {
+#     [[ $# -eq 0 ]] && echo "usage is ./fzOperateOnAllGreppedFiles [rg-options] <string>" && return 3
+#     local quickFixFile=$(mktemp -t fzf)
+#     rg --no-messages "$@" --vimgrep > "$quickFixFile"
+#     vim -q "$quickFixFile" -c ":copen"
+#     rm -f "$quickFixFile"
+# }
+# 
+# # ==== Do operation interactively on selected files in vim. 
+# function fzOperateOnSelectedFiles() {
+#     local quickFixFile=$(mktemp -t fzf)
+#     fd -H -I -t f --exclude .git | fzf_bin --multi --preview="source $UTILITY_SCRIPT; previewFileOrDir {}" --preview-window='wrap' > "$quickFixFile"
+#     vim -c ':set errorformat+=%f | :cexpr system("cat '$quickFixFile'") | copen'
+#     rm -f "$quickFixFile"
+# }
+
 # ==== Git add
 function git_add() {
     git add $(git status -s | fzf --multi --preview="source $UTILITY_SCRIPT; previewDiff {}" --preview-window='wrap' | awk '{print $2}')
