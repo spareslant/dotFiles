ac174f40d4764f767c8f9012b8049dcdcbc41ba4
Gaurav Pal <spare.slant@gmail.com>
2022-09-09 14:43:45 +0100

    git status in buffer line

--- nvim/lua/cokeline-config/init.lua
+++ nvim/lua/cokeline-config/init.lua
@@ -1,5 +1,5 @@
 local get_hex = require("cokeline.utils").get_hex
-local active_bg_color = '#393939'
+local active_bg_color = '#a5a2a2'
 local inactive_bg_color = get_hex('Normal', 'bg')
 local bg_color = get_hex('ColorColumn', 'bg')
 local is_picking_focus = require('cokeline/mappings').is_picking_focus
@@ -85,7 +85,7 @@ require('cokeline').setup({
                   if buffer.is_readonly then
                       status = '➖'
                   elseif buffer.is_modified then
-                      status = ''
+                      status = '✎'
                   end
                   return status
               end,
@@ -108,7 +108,11 @@ require('cokeline').setup({
                   if(buffer.diagnostics.errors > 0) then
                       return '#C95157'
                   else
-                      return '#b3b1ad'
+                    if buffer.is_focused then
+                      return '#040404'
+                    else
+                      return '#a5a2a2'
+                    end
                   end
               end,
               style = function(buffer)
@@ -145,10 +149,30 @@ require('cokeline').setup({
               end
           },
           {
-              text = '',
+              text = function(buffer)
+                if buffer.is_focused then
+                  if vim.b.gitsigns_status ~= nil then
+                    return vim.b.gitsigns_status
+                  end
+                end
+                return ''
+              end,
+              fg = function(buffer)
+                if vim.b.gitsigns_status ~= nil then
+                  return '#DB121B'
+                end
+              end,
+              bg = function(buffer)
+                if vim.b.gitsigns_status ~= nil then
+                  return '#ffffff'
+                end
+              end,
+          },
+          {
+              text = ' ',
               fg = function(buffer)
                 if buffer.is_focused then
-                  return '#b3b1ad'
+                  return '#040404'
                 end
               end,
               delete_buffer_on_left_click = true
