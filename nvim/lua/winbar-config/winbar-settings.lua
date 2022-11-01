385cf62ec2ec1f3b6459bacf167fb6b05593900a
Gaurav Pal <spare.slant@gmail.com>
2022-11-01 21:47:40 +0000

    a working winbar with nice color

--- nvim/lua/winbar-config/winbar-settings.lua
+++ nvim/lua/winbar-config/winbar-settings.lua
@@ -1,9 +1,27 @@
 local M = {}
 
-vim.api.nvim_set_hl(0, 'WinBarPath', { bg = '#dedede', fg = '#363636', bold = true })
-vim.api.nvim_set_hl(0, 'WinBarModified', { bg = '#dedede', fg = '#ff3838' })
+local winbar_filetype_exclude = {
+    "help",
+    "startify",
+    "dashboard",
+    "packer",
+    "neogitstatus",
+    "NvimTree",
+    "Trouble",
+    "alpha",
+    "lir",
+    "Outline",
+    "spectre_panel",
+    "toggleterm",
+}
+
+vim.api.nvim_set_hl(0, 'WinBarPath', { bg = '#a3c2be', fg = '#363636', bold = true })
+vim.api.nvim_set_hl(0, 'WinBarModified', { bg = '#a3c2be', fg = '#ff3838' })
 
 function M.eval()
+    if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
+        return ""
+    end
     local file_path = vim.api.nvim_eval_statusline('%f', {}).str
     local modified = vim.api.nvim_eval_statusline('%M', {}).str == '+' and '' or ''
 
