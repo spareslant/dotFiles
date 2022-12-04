233a3506bae020c5d0eb2678e9f862779104cb49
Gaurav Pal <spare.slant@gmail.com>
2022-12-04 11:12:15 +0000

    fixed formatting and lang server

--- nvim/lua/bufferline-config/init.lua
+++ nvim/lua/bufferline-config/init.lua
@@ -1,13 +1,13 @@
-require('bufferline').setup {
-    options = {
-        offsets = {
-            {filetype = "NvimTree", text = "File Explorer", text_align = "left"}
-        },
-        separator_style = "thick",
-        always_show_bufferline = true,
-        indicator = {
-          style = "underline",
-        },
-        color_icons = true,
-    }
-}
+require("bufferline").setup({
+  options = {
+    offsets = {
+      { filetype = "NvimTree", text = "File Explorer", text_align = "left" },
+    },
+    separator_style = "thick",
+    always_show_bufferline = true,
+    indicator = {
+      style = "underline",
+    },
+    color_icons = true,
+  },
+})
--- nvim/lua/null-ls-config/init.lua
+++ nvim/lua/null-ls-config/init.lua
@@ -5,7 +5,7 @@ local sources = {
   null_ls.builtins.formatting.prettier,
   null_ls.builtins.diagnostics.write_good,
   null_ls.builtins.code_actions.gitsigns,
-  null_ls.builtins.formatting.rustfmt,
+  -- null_ls.builtins.formatting.rustfmt,
   null_ls.builtins.formatting.shellharden,
   null_ls.builtins.formatting.stylua.with({
     extra_args = { "--indent-type=Spaces", "--indent-width=2", },
