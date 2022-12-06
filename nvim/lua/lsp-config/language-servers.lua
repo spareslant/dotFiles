b4604823b112f56b8c9570aa30491a48947faaaf
Gaurav Pal <spare.slant@gmail.com>
2022-12-06 23:30:28 +0000

    more settings

--- nvim/lua/lsp-config/language-servers.lua
+++ nvim/lua/lsp-config/language-servers.lua
@@ -1,4 +1,19 @@
 util = require("lspconfig/util")
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+
 signature_setup = require("lsp_signature").setup({
   bind = true, -- This is mandatory, otherwise border config won't get registered.
   handler_opts = {
