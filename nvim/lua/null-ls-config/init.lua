d721789960787bf9a9c688ff1fc09347464d765d
Gaurav Pal <spare.slant@gmail.com>
2022-12-18 19:11:33 +0000

    disable code actions in null-ls

--- nvim/lua/null-ls-config/init.lua
+++ nvim/lua/null-ls-config/init.lua
@@ -4,7 +4,7 @@ local null_ls = require("null-ls")
 local sources = {
   null_ls.builtins.formatting.prettier,
   -- null_ls.builtins.diagnostics.write_good,
-  null_ls.builtins.code_actions.gitsigns,
+  -- null_ls.builtins.code_actions.gitsigns,
   -- null_ls.builtins.formatting.rustfmt,
   -- null_ls.builtins.formatting.shellharden,
   null_ls.builtins.formatting.stylua.with({
