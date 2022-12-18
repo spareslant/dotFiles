e263f24125b2e93a901b7d40b620eab46af29521
Gaurav Pal <spare.slant@gmail.com>
2022-12-18 19:06:02 +0000

    disable write_good for markdown

--- nvim/lua/null-ls-config/init.lua
+++ nvim/lua/null-ls-config/init.lua
@@ -3,7 +3,7 @@ local null_ls = require("null-ls")
 -- register any number of sources simultaneously
 local sources = {
   null_ls.builtins.formatting.prettier,
-  null_ls.builtins.diagnostics.write_good,
+  -- null_ls.builtins.diagnostics.write_good,
   null_ls.builtins.code_actions.gitsigns,
   -- null_ls.builtins.formatting.rustfmt,
   -- null_ls.builtins.formatting.shellharden,
