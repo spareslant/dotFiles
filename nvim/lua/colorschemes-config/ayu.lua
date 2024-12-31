7687a6d0b21b460fb9fc23252b44140e03b7af44
Gaurav Pal <spare.slant@gmail.com>
2024-12-31 16:36:16 +0000

    color fixes

--- nvim/lua/colorschemes-config/ayu.lua
+++ nvim/lua/colorschemes-config/ayu.lua
@@ -3,7 +3,8 @@ vim.o.background = 'dark'
 require("ayu").setup({
   mirage = false,
   overrides = {
-    Comment = { fg = "#9e926f" },
+    -- Comment = { fg = "#9e926f" },
+    Comment = { fg = "#f9f29b" },
     LineNr = { fg = "#5081c0" },
     Visual = { bg = "#5a4e39" },
     CursorLine = { bg = "#2e2e2e" },
